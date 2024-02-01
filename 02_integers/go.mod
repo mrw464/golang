f653521e8b4d51e49f66a5420fe1a84d8f71313e
mrw464 <mrw464@gmail.com>
2024-02-01 18:29:50 -0500

    startin repeat

--- 02_integers/go.mod
+++
@@ -1,3 +0,0 @@
-module 02_integers
-
-go 1.21.6
---
+++ 03_iteration/repeat_test.go
@@ -0,0 +1,26 @@
+package iteration
+
+import "testing"
+
+func TestRepeat(t *testing.T) {
+	t.Run("repeat A 5 times", func(t *testing.T) {
+		repeated := Repeat("a", 5)
+		expected := "aaaaa"
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
+	t.Run("repeat b 3 times", func(t *testing.T) {
+		repeated := Repeat("b", 3)
+		expected := "bbb"
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
+	}
+}
